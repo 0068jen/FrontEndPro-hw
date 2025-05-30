305a82072ca7af256ad8b87b5d35aba58ed2d13b
Yevhenii_Tkach <0068jen@gmail.com>
2025-05-30 14:21:52 +0300

    hw6

---
+++ js/script.js
@@ -0,0 +1,57 @@
+`use strict`
+
+//this variable for the username
+const userName = 'Ivan';
+console.log(typeof userName);
+
+//this variable is for the user's age
+const age = 18;
+console.log(typeof age);
+
+//this variable is for understanding whether the user is verified
+let verificationSuccessful = true;
+console.log(typeof verificationSuccessful);
+let verificationFailed = false;
+console.log(typeof verificationFailed);
+
+//this variable for the registration date
+const dateRegistration = `12.12.2020`;
+console.log(typeof dateRegistration);
+
+//this variable for the user's card number
+const bigIntNumberUser = 123855n;
+console.log(typeof bigIntNumberUser);
+
+//this variable for the promo code
+const noPromo = null;
+console.log(typeof noPromo);
+let yesPromo = `HkOJHE1`;
+console.log(typeof yesPromo);
+
+
+//this variable for the fallback name
+let reservName;
+console.log(typeof reservName);
+
+//this variable for the number of bonuses in the account
+let bonusNumber = 156;
+console.log(typeof bonusNumber);
+
+//output user data to the console
+console.log(`
+User: ${userName}; 
+Age: ${age}; 
+Bonus: ${bonusNumber}
+`);
+
+console.log(`
+verification: ${verificationSuccessful}; 
+Promo: ${yesPromo}; 
+Date Registration: ${dateRegistration}
+`);
+
+console.log(`
+Verification: ${verificationFailed}; 
+Promo: ${noPromo}; 
+Reserv Name: ${reservName}
+`);
