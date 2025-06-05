a250e834649fc50f48053742ca0017e219bfbed8
Yevhenii_Tkach <0068jen@gmail.com>
2025-06-05 20:56:52 +0300

    hw13

---
+++ js/script.js
@@ -0,0 +1,12 @@
+`use strict`
+
+//ask the user for numbers
+const num1 = +prompt('Say the first number');
+const num2 = +prompt('Say the second number');
+const num3 = +prompt('Say the third number');
+
+//we calculate their arithmetic mean
+const averageNum = (num1 + num2 + num3) / 3;
+
+//output a message with the average value
+alert(`the average value of the given numbers is ${averageNum}`);
