802b34304fb13bea13ac8e78d7efaf573288738f
Yevhenii_Tkach <0068jen@gmail.com>
2025-06-09 14:54:25 +0300

    hw16

---
+++ js/script.js
@@ -0,0 +1,111 @@
+`use strict`
+
+// 1.Display the numbers from 10 to 20 on a single line separated by commas on the page.
+
+for (let i = 10; i <= 20; i++) {
+    console.log(i)
+};
+
+//2.Display the squares of numbers from 10 to 20.
+
+// for (let i = 10; i <= 20; i++) {
+//     console.log(i**2);
+// };
+
+//3.Display the multiplication table for 7.
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i*7);
+// };
+
+//4.Find the sum of all integers from 1 to 15.
+
+// let sum = 0;
+
+// for (let i = 1; i <= 15; i++) {
+//     sum = sum + i
+// };
+
+// console.log(sum);
+
+//5.Find the product of all integers from 15 to 35.
+
+// let product = 15n;
+//
+// for (let i = 16n; i <= 35n; i++) {
+//     product = product * i
+// };
+
+// console.log(product);
+
+//6.Find the arithmetic mean of all integers from 1 to 500.
+
+// let arithmeticMean = 0;
+//
+// let sum = 0;
+//
+// for (let i = 1; i <= 500; i++) {
+//     sum = sum + i
+// };
+//
+// arithmeticMean = sum / 500;
+//
+// console.log(arithmeticMean);
+
+//7.Print the sum of only even numbers in the range from 30 to 80.
+
+// let sum = 0;
+//
+// for (let i = 30; i <= 80; i++) {
+//     sum = (i % 2 === 0) ? sum + i : sum;
+// };
+//
+// console.log(sum);
+
+//8.Output all numbers in the range from 100 to 200 that are multiples of 3.
+
+// for (let i = 100; i <= 300; i++) {
+//     (i % 3 === 0) ? console.log(i) : null;
+// };
+
+//9.Given a natural number, find and print all its divisors.
+
+// let naturalNumber = +prompt("Enter a natural number");
+//
+// for (let i = 1; i <= naturalNumber; i++) {
+//     (naturalNumber % i === 0) ? console.log(i) : null;
+// };
+
+//10.Determine the number of its even divisors.
+
+// let naturalNumber = +prompt("Enter a natural number");
+//
+// let number = 0;
+//
+// for (let i = 1; i <= naturalNumber; i++) {
+//     if (naturalNumber % i === 0 && i % 2 ===0) {number++}
+// };
+//
+// console.log(number);
+
+//11.Find the sum of its even divisors.
+
+// let naturalNumber = +prompt("Enter a natural number");
+//
+// let sum = 0;
+//
+// for (let i = 1; i <= naturalNumber; i++) {
+//     if (naturalNumber % i === 0 && i % 2 ===0) {sum = sum + i}
+// };
+
+// console.log(sum);
+
+//12. Print the complete multiplication table from 1 to 10.
+
+// for (let i = 1; i <= 10; i++) {
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i * j}`)
+//     }
+// };
+
+
